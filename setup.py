3ad2710266b6433d4960eda5ef6e8b405b9373aa
brinias <brinias2@gmail.com>
2025-06-27 16:29:09 +0200

    :)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="brinias", 
+    name="brinias-engine", 
     version="0.1.0",
     author="Konstantinos Brinias",
     author_email="brinias2@gmail.com",
